9fd1dc60939e9ac7f289f8460127b06322c9d64b
enswbl <55461135+enswbl@users.noreply.github.com>
2021-07-22 06:29:55 +0500

    hotfix: убрать required у avatar

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -3,7 +3,7 @@ const { celebrate, Joi } = require('celebrate');
 const { RegExp } = require('../config');
 
 const createUserValidation = celebrate({
-  body: Joi.object().keys({ // TODO
+  body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(RegExp.URL),
